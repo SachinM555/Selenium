a96b199e928f65947878f43fc37b745343649bec
SachinM555 <sachin.qa4it@gmail.com>
2016-08-23 21:00:59 +0530

    First commit

---
+++ Sspkm/src/spkm/SPK.java
@@ -0,0 +1,23 @@
+package spkm;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SPK {
+ 
+  public WebDriver driver;
+ 
+  @Test
+  public void main() {
+	  
+      driver = new FirefoxDriver();
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      driver.get("http://www.google.co.in");
+      
+      driver.manage().window().maximize();
+      driver.quit();
+      
+  }
+}
